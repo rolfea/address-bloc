c31eec65bd2910e9b506bb5ed65916cc7b01efcd
Alex Rolfe <rolfepercussion@gmail.com>
2016-09-22 09:44:18 -0500

    Adds start of the method_missing to Entry.rb

--- models/entry.rb
+++ models/entry.rb
@@ -5,4 +5,8 @@ class Entry < BlocRecord::Base
   def to_s
     "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
   end
+
+  def method_missing(method, *args, &block)
+    
+  end
 end
